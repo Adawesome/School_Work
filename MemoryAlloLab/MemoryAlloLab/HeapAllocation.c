c52caa5b8624b89464b6fd4ea33bfe08b505d6e8
Adawesome <adam.ccmpbll@gmail.com>
2018-03-06 16:57:11 -0800

    Most of my_alloc done
    One remaining else - logic then proceed to testing of my_alloc

--- MemoryAlloLab/MemoryAlloLab/HeapAllocation.c
+++ MemoryAlloLab/MemoryAlloLab/HeapAllocation.c
@@ -11,24 +11,92 @@ void my_initialize_heap(int size) {
 	//4 bytes for pointer, 4 bytes for size
 	//size - 8.
 	free_head = malloc(size);
-	(*free_head).block_size = size - 8;
-	(*free_head).next_block = 0;
+	(*free_head).block_size = size - overheadSize;
+	(*free_head).next_block = NULL;
 }
 void* my_alloc(int size) {
-	//size must be positive
-	//block must be multiple of void*
-	//walk freelist starting with head w/ first fit heuristic
-	//on block found, need to split(can fit wanted size AND excess to fit a new block with overhead)?
-	//if no split, redirect pointers (remove block to fill from "singly linked list")
-	//account for if it is head or not (removing is different)
-	//byte location of new block = current block + allocation request
-	//init new block
-	//return pointer to data region which is overhead size
-
-	if (size > 0 && (overheadSize % voidSize == 0)) {
-		//walk freelist starting with head
-
+	//alignment rules: struct is size 8, declare b4 ints
+	struct Block* myBlock = free_head;
+	struct Block* prevBlock = free_head;
+	int multiple = size % voidSize;
+	int countToPrevious = 0;
+	//mutate: "True size to allocate"
+	//int mutate = size + (size % voidSize);
+	int mutate;
+	// probably don't need this if/else statement
+	if (multiple != 0) {
+		mutate += size + multiple;
 	}
+	else
+		mutate = size;
+	//start "singly linked list" traversal
+	while(!(*myBlock).block_size >= mutate){
+		//this means that myBlocks size does not have enough room to
+		//fit the desired allocation size (parameter size)
+		if(myBlock == NULL) {
+			// can't do anything, return to end my_alloc
+			return 0;
+		}
+		else
+		{
+			//move to next block
+			myBlock = (*myBlock).next_block;
+			countToPrevious++;
+		}
+		
+	}
+	for (int i = 0; i < countToPrevious; i++) {
+		//move prevBlock to be the previous Block that would point to myBlock.
+		prevBlock = (*prevBlock).next_block;
+	}
+	//quick test to check prev Block/////////
+	if((*prevBlock).next_block == myBlock)
+		printf("We Gucci...\n");
+	//////////////////end test/////////////
+
+	//leaving this loop means the block size is good.
+	//if yes AND can split block
+	int checkSplit = (*myBlock).block_size - mutate;
+	if (checkSplit > (overheadSize + voidSize)) {
+		//above logic: if checksplit is greater than compared number 
+		//then it has remaining space to make a new block
+		//now check if myBlock is same as free head or not
+		if (myBlock == free_head)
+		{
+			//change free_head to point to the new splitted block.
+			free_head = overheadSize + mutate;
+			//ensure free_head still points to NULL or open blocks
+			(*free_head).next_block = (*myBlock).next_block;
+			//fix block size of my block
+			(*myBlock).block_size = mutate;
+		}
+		else {
+			//unlock above, myBlock will need to point to the newly split block to properly
+			//rearrange pointers
+			//this means that we are not same as free_head, but traversed the "list"
+			(*prevBlock).next_block = (*myBlock).next_block;
+			(*myBlock).block_size = mutate;
+			// \/create split below \/
+			myBlock = overheadSize + mutate;
+			//^^create split above^^
+			(*myBlock).next_block = free_head;
+			free_head = myBlock;
+		}
+	}//if yes but cannot split block
+	else {
+		//implement allocation
+		if (myBlock == free_head) {
+			//no need to move pointers other than head
+			(*myBlock).block_size = mutate;
+			free_head = (*myBlock).next_block;
+		}
+		else {
+			//will require pointer changing unlike above if
+
+		}
+	}
+
+
 }
 int main() {
 	//swag 
