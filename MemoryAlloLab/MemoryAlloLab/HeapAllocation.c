ffd48fc1222a543f950542ee2c9bd1acfe35ae3d
Adawesome <adam.ccmpbll@gmail.com>
2018-03-12 12:09:48 -0700

    Final Commit w/ project fin

--- MemoryAlloLab/MemoryAlloLab/HeapAllocation.c
+++ MemoryAlloLab/MemoryAlloLab/HeapAllocation.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct Block {
 	int block_size; //# of bytes in the data section
 	struct Block *next_block; //in C, you have to use 'struct Block' as the type
 };
+
+struct Block *free_head;
 const int overheadSize = sizeof(struct Block);
 const int voidSize = sizeof(void*);
-struct Block *free_head;
+
 void my_initialize_heap(int size) {
 	//4 bytes for pointer, 4 bytes for size
 	//size - 8.
 	free_head = malloc(size);
-	printf("fresh malloc free_head at: %p\n", (void *)free_head);
+	//printf("fresh malloc free_head at: %p\n", (void *)free_head);
 	//(*free_head).block_size = size - overheadSize; <<< this created a "ghost" overhead of 8bytes
 	(*free_head).block_size = size;
 	(*free_head).next_block = NULL;
 }
+
 void* my_alloc(int size) {
 	//alignment rules: struct is size 8, declare b4 ints
 	struct Block* myBlock = free_head;
@@ -24,9 +28,8 @@ void* my_alloc(int size) {
 	int countToPrevious = 0;
 	//mutate: "True size to allocate"
 	//int mutate = size + (size % voidSize);
-	int mutate;
-	// probably don't need this if/else statement
-	mutate = size;
+	//I manipulate mutate to accommodate for voidSize in order to keep the purity of size.
+	int mutate = size;
 	while (multiple != 0) {
 		mutate++;
 		multiple = mutate % voidSize;
@@ -49,20 +52,19 @@ void* my_alloc(int size) {
 			myBlock = (*myBlock).next_block;
 			countToPrevious++;
 		}
-		
 	}
 	for (int i = 1; i < countToPrevious; i++) {
 		//move prevBlock to be the previous Block that would point to myBlock.
 		prevBlock = (*prevBlock).next_block;
 	}
-	//leaving this loop means the block size is good.
-	//if yes AND can split block
+	//check to be doubly sure.
 	if (myBlock == NULL) {
 		// can't do anything, return to end my_alloc
 		return 0;
 	}
 	int checkSplit = (*myBlock).block_size - mutate;
-	if (checkSplit >= (overheadSize + voidSize)) {
+	//if true then can split block
+	if (checkSplit > (overheadSize + voidSize)) {
 		//above logic: if checksplit is greater than compared number 
 		//then it has remaining space to make a new block
 		//now check if myBlock is same as free head or not
@@ -75,8 +77,7 @@ void* my_alloc(int size) {
 			(*free_head).next_block = (*myBlock).next_block;
 			//fix block size of my block
 			(*free_head).block_size = (*myBlock).block_size - overheadSize - mutate;
-			(*myBlock).block_size = mutate;
-			
+			(*myBlock).block_size = mutate;//need this because my_free takes a peek at block_size
 		}
 		else {
 			//unlock above, myBlock will need to point to the newly split block to properly
@@ -94,55 +95,56 @@ void* my_alloc(int size) {
 			(*myBlock).next_block = (*prevBlock).next_block;
 			//finally have prevBlock point to the newly opened block
 			(*prevBlock).next_block = myBlock;
-			(*myBlock).block_size = splitSize;
-			return tempBlockTest;
+			(*myBlock).block_size = splitSize;//only time need to change myBlock block_size
+			return tempBlockTest; //we lose what was allocated because we move myBlock so need this temp
 		}
-	}//if yes but cannot split block
+	}
 	else {
-
+		//Cannot split block
 		//implement allocation
 		if (myBlock == free_head) {
 			//no need to move pointers other than head
-			//(*myBlock).block_size = mutate;
+			(*myBlock).block_size = mutate;
 			free_head = (*myBlock).next_block;
 		}
 		else {
-			//will require pointer changing unlike above if
+			//CANNOT SPLIT, IN MIDDLE OF LIST
+			//counter of what is told of me in class, I'm leaving in blockSize = mutate because of how my_free works.
+			(*myBlock).block_size = mutate;
 			(*prevBlock).next_block = (*myBlock).next_block;
-			//(*myBlock).block_size = mutate;
-			//(*myBlock).next_block = NULL;
 		}
 	}
 	return myBlock;
 }
+
 void my_free(void *data) {
 	// will deallocate the given Block pointer (passed via parameter)
 	//note, a pointer is being passed as a parameter... this is pass by reference... not changing
 	//a "copy" of the value but the actual value itself
 	struct Block* blockLocation = data;
 	(*blockLocation).block_size += overheadSize;
-	//fix block size becuase we create overhead on my_alloc, so have to get rid of that overhead here
-	//otherwise we would have 2 overheads...
+	//adjust block size becuase we create overhead on my_alloc that isn't represented in block_size, but we have to get rid of that overhead here
+	//otherwise we would have 2 overheads... ^^I'm sure most may not have this, but I neeeeed it
 	(*blockLocation).next_block = free_head;
 	free_head = blockLocation;
 	//1st : "data" points to current_free_head
 	//2nd : free_head points to "data" which is pointing to old_free_head
 
 }
+
 void testCase1() {
-	//my_initialize_heap(1000);
 	printf("TESTCASE ONE\n");
 	printf("______________________________________\n");
 	void* ptr = my_alloc(sizeof(int));
 	printf("CURRENT POINTER: %p\n", (void *)ptr);
 	my_free(ptr);
-	printf("AFTER FREE: %p\n", (void *)ptr);
+	//printf("AFTER FREE: %p\n", (void *)ptr);
 	void* next = my_alloc(sizeof(int));
 	printf("SAME ALLOC: %p\n", (void *)next);
 	printf("______________________________________\n");
 }
+
 void testCase2() {
-	//my_initialize_heap(1000);
 	printf("TESTCASE TWO\n");
 	printf("______________________________________\n");
 	//overheadSize == 8
@@ -154,10 +156,11 @@ void testCase2() {
 	void* allo2 = my_alloc(sizeof(int));
 	printf("SECOND INT POINTER: %p\n", (void *)allo2);
 	//printPointer(allo2);
+	printf("FREE_HEAD POINTER : %p\n", (void *)free_head);
 	printf("______________________________________\n");
 }
+
 void testCase3() {
-	//my_initialize_heap(1000);
 	int temp;
 
 	printf("TESTCASE THREE\n");
@@ -185,15 +188,17 @@ void testCase3() {
 	temp = printPointer(sameAs2);
 	printf("CURRENT FREE HEAD : ");
 	temp = printPointer(free_head);
+	printf("free_head should be 16 bytes (my hex's are in bits) ahead of allo4\n");
 	printf("______________________________________\n");
 }
+
 int printPointer(void *ptr) {
 	struct Block* test = ptr;
 	printf("%p\n",(void *)ptr);
 	return 0;
 }
+
 void testCase4() {
-	//my_initialize_heap(1000);
 	printf("TESTCASE FOUR\n");
 	printf("______________________________________\n");
 	int temp;
@@ -209,8 +214,8 @@ void testCase4() {
 	printPointer(free_head);
 	printf("______________________________________\n");
 }
+
 void testCase5() {
-	//my_initialize_heap(1000);
 	//int arr[100];
 	printf("TESTCASE FIVE\n");
 	printf("______________________________________\n");
@@ -229,6 +234,7 @@ void testCase5() {
 	printPointer(free_head);
 	printf("______________________________________\n");
 }
+
 int main() {
 	my_initialize_heap(1000);
 	//testCase1();
