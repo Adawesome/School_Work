c30bc8096654e31ebf84e452fb2f042bfff3bcd7
Adawesome <adam.ccmpbll@gmail.com>
2018-04-23 01:18:51 -0700

    Finished Monte Carlo Fsharp

--- 424-Programming Languages and Theory/Monte Carlo and Railway Lab/MonteCarlo.fs
+++ 424-Programming Languages and Theory/Monte Carlo and Railway Lab/MonteCarlo.fs
@@ -1,28 +1,35 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+//
+// type:Point - DUH!
 type Point = { X: float; Y: float; }
+
+//type HitResult - DUH!
 type HitResult =
     | Hit
     | Miss of float
+
+// type: float->float->Point (takes 2 floats, returns 1 Point)
 let makePoint x y =
     {X = x; Y = y;}
-type System.Random with
-    /// Generates an infinite sequence of random numbers within the given range.
-    member this.GetValues(minValue, maxValue) =
-        Seq.initInfinite (fun _ -> this.Next(minValue, maxValue))
+
 
 let r = System.Random()
-let randomPoint (ran:System.Random) =
+
+// type: unit -> Point (work around cuz of random seeding, have mercy!)
+let randomPoint() =
     let x = r.NextDouble()
     let y = r.NextDouble()
     makePoint x y
-//recursively con to a list of randomPoints
-let throwDarts n =
-    List.init n (fun index -> randomPoint r)
 
+//type: int -> Point list 
+let throwDarts n =
+    List.init n (fun index -> randomPoint())
+
+// lil function i use in counthits to print out, as implied, length of list...
+// 'a list -> unit (had to look this one up... didn't really know what a printfn would output... spare me!)
 let listLength coll =
      coll |> List.length |> printfn "List of list is: %d"
 
+//type: Point -> HitResult (i forget why i had to ensure point was Point i think it was to make the compiler happy or something dumb like that)
 let isHit (point:Point) = 
     let circleBarrier = 
         (pown point.X 2) + (pown point.Y 2)
@@ -32,7 +39,8 @@ let isHit (point:Point) =
     else
         HitResult.Miss (circleBarrier - 1.0)
 
-//HitResult list -> int
+//type: HitResult list -> int -> int, a helper function I wrote to be used in CountHits... takes (1 HitResultList and 1 int and returns 1 int)
+// makes use of some sexy tail end recursion (+1 extra credit maybe?)
 let rec helper coll value =
     if List.isEmpty coll then
         value
@@ -45,22 +53,16 @@ let rec helper coll value =
             helper (coll |> List.tail) (value + 1)
         | _ ->
             helper (coll |> List.tail) (value)
-    
-//    match coll with
-//    | []  -> value
-//    | [Hit] ->
-//        printfn "making recursion: %d" value
-//        helper (coll |> List.tail ) (value + 1.0)
-//    | _ ->
-//            helper (coll |> List.tail) value
+
 
 // int -> int
-let rec countHits n =
+//note the use of the above helper function which is of type HitResult list -> int -> int
+let countHits n =
     let dartList = throwDarts n |> List.map (fun x -> isHit x)
     listLength dartList
     helper dartList 0
 
-// int -> int
+// int -> float
 let estimatePi n =
     let numHits = countHits n
     printfn "number of counted hits: %d" numHits
